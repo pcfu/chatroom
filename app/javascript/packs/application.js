f49f35c8b9074f323b66f7fce353a1cbb3a297cf
Peichong <fupeichong@gmail.com>
2021-03-02 10:27:12 +0800

    add font-awesome-5 via yarn

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,6 +4,7 @@
 // that code so it'll be compiled.
 
 import 'bootstrap'
+import "@fortawesome/fontawesome-free/js/all"
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
@@ -13,7 +14,6 @@ require("channels")
 const jQuery = require('jquery')
 global.$ = global.jQuery = jQuery;
 window.$ = window.jQuery = jQuery;
-
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
