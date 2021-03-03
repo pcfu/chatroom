ac4f7cafd1224dc4f2a978c75e057e3f550a017e
pcfu <fupeichong@github.com>
2021-03-03 18:31:14 +0800

    minor code corrections

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get register" do
-    get register_url
+    get register_path
     assert_response :success
     assert_select 'title', "#{Globals::App::TITLE} | Registration"
   end
