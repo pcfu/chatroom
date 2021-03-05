143a6d30ffbcf4a897efc85e882daf0d40a4481a
pcfu <fupeichong@gmail.com>
2021-03-05 18:40:37 +0800

    add styleable_select tag helper and css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,31 @@ module ApplicationHelper
     title
   end
 
+  def styleable_select_tag(choices, options = {})
+    wrapper_opts = {class: 'styleable-select-wrapper'}
+    wrapper_opts[:id] = options[:id] if options[:id].present?
+    prompt = options[:prompt].present? ? options[:prompt].capitalize : ''
+
+    tag.div(wrapper_opts) do
+      tag.div(class: 'styleable-select') do
+        content = tag.div(class: 'styleable-select-trigger') do
+          tag.span(prompt, class: 'styleable-select-prompt').concat(
+            tag.div(class: 'styleable-select-toggle') {tag.i class:'fas fa-caret-down'}
+          )
+        end
+
+        choices = Hash[choices.zip(choices)] if choices.is_a? Array
+        content.concat(
+          tag.div(class: 'styleable-select-options') do
+            choices.collect do |data, value|
+              concat(tag.span(value, class: 'styleable-option', 'data-value': data))
+            end
+          end
+        )
+
+        content
+      end
+    end
+  end
+
 end
