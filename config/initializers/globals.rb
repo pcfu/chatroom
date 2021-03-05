c820ae6b907f6253c6d16006a9374b14be0e4693
pcfu <fupeichong@gmail.com>
2021-03-05 18:40:10 +0800

    update globals

--- config/initializers/globals.rb
+++ config/initializers/globals.rb
@@ -2,6 +2,8 @@ module Globals
   class App
     TITLE = 'Chatroom'
     MIN_USER_AGE = 18
+    MAX_USER_AGE = 150
     MAX_DOB = Date.current.advance(years: -MIN_USER_AGE)
+    MIN_DOB = Date.current.advance(years: -MAX_USER_AGE)
   end
 end
