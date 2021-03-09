ecde73f10145b5516637e66641784d4f012eabe6
pcfu <fupeichong@gmail.com>
2021-03-09 17:00:47 +0800

    Users controller - create user on registration submit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,15 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    @user.save
+
     render 'register'
   end
+
+  private
+
+    def user_params
+      params.require(:user).permit(*User.create_keys)
+    end
 end
