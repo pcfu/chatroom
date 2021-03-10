493a6df5a6d786d41374379a07a0ae7134648278
pcfu <fupeichong@gmail.com>
2021-03-10 09:47:50 +0800

    move globals into app/lib

--- app/lib/globals.rb
+++ app/lib/globals.rb
@@ -1,9 +1,9 @@
 module Globals
-  class App
+  module App
     TITLE = 'Chatroom'
   end
 
-  class Tooltip
+  module Tooltip
     AGE_LIMIT = "Users must be at least #{User::MIN_USER_AGE} years of age."
     PW_REQS = "Password must be #{User::MIN_PW_LEN}-#{User::MAX_PW_LEN} characters " +
               "and contain at least 1 letter, 1 number, and 1 special character."
