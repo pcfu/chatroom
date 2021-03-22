5df2a56182d1bee39a528894630abe6412316aac
Peichong <fupeichong@gmail.com>
2021-03-22 19:34:11 +0800

    update docker config

--- .dockerdev/setup.sh
+++ .dockerdev/setup.sh
@@ -3,7 +3,6 @@ VERS=`grep -i 'project_version' .env | cut -f2- -d=`
 
 BUNDLE=${NAME}_${VERS}_bundle
 NODE_MODULES=${NAME}_${VERS}_node_modules
-PACKS=${NAME}_${VERS}_packs
 
 output=$({ `docker volume inspect $BUNDLE`; } 2>&1)
 if [[ "$output" == *"No such volume"* ]]; then
@@ -13,11 +12,6 @@ fi
 output=$({ `docker volume inspect $NODE_MODULES`; } 2>&1)
 if [[ "$output" == *"No such volume"* ]]; then
     docker volume create $NODE_MODULES
-fi
-
-output=$({ `docker volume inspect $PACKS`; } 2>&1)
-if [[ "$output" == *"No such volume"* ]]; then
-    docker volume create $PACKS
 fi
 
 docker-compose build
