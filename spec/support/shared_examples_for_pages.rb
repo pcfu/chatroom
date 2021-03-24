5cf706f7ef6afecd453fd86ffa6249305dfc9c01
Peichong <fupeichong@gmail.com>
2021-03-24 19:21:37 +0800

    add spec for navbar

--- spec/support/shared_examples_for_pages.rb
+++ spec/support/shared_examples_for_pages.rb
@@ -41,25 +41,5 @@ RSpec.shared_examples_for 'static page' do
         expect(page).to have_no_link('Login', href: '#')
       end
     end
-
-    it "expands navbar on menu click", js: true do
-      find('button.navbar-toggler').click
-      expect(page).to have_css('a.navbar-brand[href="/"]')
-      expect(page).to have_link('About', href: '#')
-      expect(page).to have_link('Features', href: '#')
-      expect(page).to have_link('Sample Link', href: '#')
-      expect(page).to have_link('Login', href: '#')
-      expect(page).to have_css('button.close')
-    end
-
-    it "collapses navbar on close click", js: true do
-      find('button.navbar-toggler').click
-      find('button.close').click
-      expect(page).to have_no_link('About', href: '#')
-      expect(page).to have_no_link('Features', href: '#')
-      expect(page).to have_no_link('Sample Link', href: '#')
-      expect(page).to have_no_link('Login', href: '#')
-      expect(page).to have_no_css('button.close')
-    end
   end
 end
---
+++ spec/system/navbar_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe "Navbar", type: :system, js: true do
+  context "when collapsed" do
+    before do
+      resize_window_to_small
+      visit root_path
+    end
+    after { resize_window_to_default }
+
+    it "expands navbar on menu click" do
+      find('button.navbar-toggler').click
+      expect(page).to have_css('a.navbar-brand[href="/"]')
+      expect(page).to have_link('About', href: '#')
+      expect(page).to have_link('Features', href: '#')
+      expect(page).to have_link('Sample Link', href: '#')
+      expect(page).to have_link('Login', href: '#')
+      expect(page).to have_css('button.close')
+    end
+
+    it "collapses navbar on close click", js: true do
+      find('button.navbar-toggler').click
+      find('button.close').click
+      expect(page).to have_no_link('About', href: '#')
+      expect(page).to have_no_link('Features', href: '#')
+      expect(page).to have_no_link('Sample Link', href: '#')
+      expect(page).to have_no_link('Login', href: '#')
+      expect(page).to have_no_css('button.close')
+    end
+  end
+end
