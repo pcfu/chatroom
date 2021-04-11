5a95710f3cf77a627d70a5e6d475b32420e0a5a4
Peichong <fupeichong@gmail.com>
2021-04-11 18:01:48 +0800

    move request spec for user registration under users spec

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe "Statics", type: :request do
-  describe "GET /root" do
+RSpec.describe "Users", type: :request do
+  describe "GET /register" do
     it "responds successfully" do
-      get root_path
+      get register_path
       expect(response).to have_http_status '200'
     end
   end
