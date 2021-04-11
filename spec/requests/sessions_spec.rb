8bf7cfffe2db71d840e1335b92396e25803be6e4
Peichong <fupeichong@gmail.com>
2021-04-11 23:56:04 +0800

    refactor request specs

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe "Statics", type: :request do
-  describe "GET /root" do
+RSpec.describe "Sessions", type: :request do
+  describe "GET /login" do
     it "returns http success" do
-      get root_path
+      get login_path
       expect(response).to have_http_status(:success)
     end
   end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
   describe "GET /register" do
-    it "responds successfully" do
+    it "returns http success" do
       get register_path
-      expect(response).to have_http_status '200'
+      expect(response).to have_http_status(:success)
     end
   end
 end
