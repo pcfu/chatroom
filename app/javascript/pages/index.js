134fba9715d7c3b8984e05dfade832e1a8c7f31a
Peichong <fupeichong@gmail.com>
2021-04-15 06:55:19 +0800

    minor refactor

--- app/javascript/pages/index.js
+++ app/javascript/pages/index.js
@@ -1,12 +1,12 @@
 import { loginFns } from './login';
 import { registerFns } from './register';
 
-const fns = {
+const FUNCS = {
   login: loginFns,
   register: registerFns
 };
 
 $(document).on('turbolinks:load', function () {
   const page = window.location.pathname.replace('/', '');
-  if (fns[page]) fns[page]();
+  if (FUNCS[page]) FUNCS[page]();
 });
