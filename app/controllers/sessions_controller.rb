ce7eb98cd18bce520fb53fccbd5fa5b6a1e0f2c0
Peichong <fupeichong@gmail.com>
2021-04-15 06:55:49 +0800

    add login authentication controller action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,28 @@ class SessionsController < ApplicationController
   end
 
   def create
-    target = User.where(username: params[:username]).first
-    if target.present?
-      redirect_to chatroom_url
-    else
-      render :new
-    end
+    params = create_params
+    response = authenticate_user(params[:username], params[:password])
+    render response
   end
+
+  private
+
+    def create_params
+      params.require(:session).permit(:username, :password)
+    end
+
+    def authenticate_user(username, password)
+      user = User.where(username: username).first
+
+      if user.present?
+        if user.authenticate(password)
+          Hash[:json => nil, :status => :ok]
+        else
+          Hash[:json => { field: :password, msg: 'incorrect password' }, :status => :unauthorized]
+        end
+      else
+        Hash[:json => { field: :username, msg: 'not recognized' }, :status => :unauthorized]
+      end
+    end
 end
