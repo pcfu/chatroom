5bf170b26a55a4db4c00f5bac0ccd75af7aac080
Peichong <fupeichong@gmail.com>
2021-04-15 08:02:39 +0800

    add login request spec

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -7,4 +7,37 @@ RSpec.describe "Sessions", type: :request do
       expect(response).to have_http_status(:success)
     end
   end
+
+  describe "POST /login" do
+    context "when valid login" do
+      it "returns http success" do
+        create(:user)
+        attrs = attributes_for(:user)
+        params = {
+          session: {
+            username: attrs[:username],
+            password: attrs[:password]
+          }
+        }
+
+        post '/login', params: params
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context "when invalid login" do
+      it "returns http unauthorized" do
+        attrs = attributes_for(:user)
+        params = {
+          session: {
+            username: attrs[:username],
+            password: attrs[:password]
+          }
+        }
+
+        post '/login', params: params
+        expect(response).to have_http_status(:unauthorized)
+      end
+    end
+  end
 end
