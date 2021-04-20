803647443ad3fdaa55e144c0e78c265cc4e1d918
Peichong <fupeichong@gmail.com>
2021-04-20 18:22:50 +0800

    sessions spec - minor refactor

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -12,30 +12,18 @@ RSpec.describe "Sessions", type: :request do
     context "when valid login" do
       it "returns http success" do
         create(:user)
-        attrs = attributes_for(:user)
-        params = {
-          session: {
-            username: attrs[:username],
-            password: attrs[:password]
-          }
-        }
-
+        params = { session: attributes_for(:user).extract!(:username, :password) }
         post '/login', params: params
+        expect(controller.logged_in?).to be true
         expect(response).to have_http_status(:success)
       end
     end
 
     context "when invalid login" do
       it "returns http unauthorized" do
-        attrs = attributes_for(:user)
-        params = {
-          session: {
-            username: attrs[:username],
-            password: attrs[:password]
-          }
-        }
-
+        params = { session: attributes_for(:user).extract!(:username, :password) }
         post '/login', params: params
+        expect(controller.logged_in?).to be false
         expect(response).to have_http_status(:unauthorized)
       end
     end
