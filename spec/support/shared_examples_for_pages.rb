2d37cce01eb50be163d0d205175d637789921a0d
Peichong <fupeichong@gmail.com>
2021-04-30 07:52:12 +0800

    trim down specs for navbar state in shared examples

--- spec/support/shared_examples_for_pages.rb
+++ spec/support/shared_examples_for_pages.rb
@@ -4,20 +4,7 @@ RSpec.shared_examples_for 'static page' do
     after { resize_window_to_default }
 
     it "has an expanded navbar", js: true do
-      expect(page).to have_css('body > nav', count: 1)
-      expect(page).to have_css('body > div', count: 1)
-      expect(page).to have_css('body > nav:nth-child(1).navbar')
-      expect(page).to have_css('body > div:nth-child(2).container')
-
-      within('nav > div.container') do
-        expect(page).to have_css('a.navbar-brand[href="/"]')
-        expect(page).to have_link('About', href: '#')
-        expect(page).to have_link('Features', href: '#')
-        expect(page).to have_link('Sample Link', href: '#')
-        expect(page).to have_link('Login', href: '/login')
-
-        expect(page).to have_no_css('button.navbar-toggler')
-      end
+      expect(page).to have_css('#navbar-collapsible')
     end
   end
 
@@ -26,20 +13,7 @@ RSpec.shared_examples_for 'static page' do
     after { resize_window_to_default }
 
     it "has a collapsed navbar", js: true do
-      expect(page).to have_css('body > nav', count: 1)
-      expect(page).to have_css('body > div', count: 1)
-      expect(page).to have_css('body > nav:nth-child(1).navbar')
-      expect(page).to have_css('body > div:nth-child(2).container')
-
-      within('nav > div.container') do
-        expect(page).to have_css('a.navbar-brand[href="/"]')
-        expect(page).to have_css('button.navbar-toggler')
-
-        expect(page).to have_no_link('About', href: '#')
-        expect(page).to have_no_link('Features', href: '#')
-        expect(page).to have_no_link('Sample Link', href: '#')
-        expect(page).to have_no_link('Login', href: '#')
-      end
+      expect(page).to have_no_css('#navbar-collapsible')
     end
   end
 end
