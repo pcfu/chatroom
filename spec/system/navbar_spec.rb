f37df9e8795010a93a288ef4f6026517128b1447
Peichong <fupeichong@gmail.com>
2021-04-30 07:53:13 +0800

    update navbar spec

--- spec/system/navbar_spec.rb
+++ spec/system/navbar_spec.rb
@@ -8,24 +8,90 @@ RSpec.describe "Navbar", type: :system, js: true do
     end
     after { resize_window_to_default }
 
-    it "expands navbar on menu click" do
-      find('button.navbar-toggler').click
-      expect(page).to have_css('a.navbar-brand[href="/"]')
-      expect(page).to have_link('About', href: '#')
-      expect(page).to have_link('Features', href: '#')
-      expect(page).to have_link('Sample Link', href: '#')
-      expect(page).to have_link('Login', href: '/login')
-      expect(page).to have_css('button.close')
+    context "when menu closed" do
+      it "has minimal menu links" do
+        expect(page).to have_css('a.navbar-brand[href="/"]')
+        expect(page).to have_css('button.navbar-toggler')
+        expect_no_static_pages_links
+      end
+
+      it "opens menu on toggler click" do
+        find('button.navbar-toggler').click
+        expect(page).to have_css('#navbar-collapsible.show')
+        expect(page).to have_css('a.navbar-brand[href="/"]')
+        expect_static_pages_links
+        expect(page).to have_css('button.close')
+      end
     end
 
-    it "collapses navbar on close click", js: true do
-      find('button.navbar-toggler').click
-      find('button.close').click
-      expect(page).to have_no_link('About', href: '#')
-      expect(page).to have_no_link('Features', href: '#')
-      expect(page).to have_no_link('Sample Link', href: '#')
-      expect(page).to have_no_link('Login', href: '/login')
-      expect(page).to have_no_css('button.close')
+    context "when menu open" do
+      before { find('button.navbar-toggler').click }
+
+      it "closes menu on close click", js: true do
+        find('button.close').click
+        expect(page).to have_no_css('#navbar-collapsible.show')
+        expect_no_static_pages_links
+        expect(page).to have_no_css('button.close')
+      end
+
+      context "when logged out" do
+        it "has full links with login button" do
+          expect(page).to have_css('a.navbar-brand[href="/"]')
+          expect(page).to have_css('button.close')
+          expect_static_pages_links
+          expect(page).to have_link('Login', href: '/login')
+        end
+      end
+
+      context "when logged in" do
+        let(:user) { create :user }
+        before do
+          gui_login_user user
+          find('button.navbar-toggler').click
+        end
+
+        it "has full links with user controls" do
+          expect(page).to have_css('a.navbar-brand[href="/"]')
+          expect(page).to have_css('button.close')
+          expect_static_pages_links
+
+          expect(page).to have_css('img.gravatar')
+          expect(page).to have_css('.navbar-username', text: user.username)
+          expect(page).to have_link('Account', href: '#')
+          expect(page).to have_link('Logout', href: '/logout')
+        end
+      end
+    end
+  end
+
+  context "when expanded" do
+    before do
+      resize_window_to_medium
+      visit '/'
+    end
+
+    it "has no menu toggler" do
+      expect(page).to have_no_css('button.navbar-toggler')
+    end
+
+    context "when logged out" do
+      it "has full links with login button" do
+        expect(page).to have_css('a.navbar-brand[href="/"]')
+        expect_static_pages_links
+        expect(page).to have_link('Login', href: '/login')
+      end
+    end
+
+    context "when logged in" do
+      let(:user) { create :user }
+      before { gui_login_user user }
+
+      it "has full links with user profile" do
+        expect(page).to have_css('a.navbar-brand[href="/"]')
+        expect_static_pages_links
+        expect(page).to have_css('img.gravatar')
+        expect(page).to have_css('.navbar-username', text: user.username)
+      end
     end
   end
 end
