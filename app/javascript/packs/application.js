92e24e872419a6078079d1a91d387a72482b7b3b
Peichong <fupeichong@gmail.com>
2021-06-02 07:56:37 +0800

    use Webpacker instead of Sprockets for images

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -25,8 +25,8 @@ window.$ = window.jQuery = jQuery;
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+const images = require.context('../images', true);
+const imagePath = (name) => images(name, true);
 
 // Enable Bootstrap tooltips everywhere
 $(function () {
