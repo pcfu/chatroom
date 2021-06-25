49f4210b327d9e24671806cca6837b3e90dd2145
Peichong <fupeichong@gmail.com>
2021-06-25 08:08:08 +0800

    navbar - hide open user panel when clicking outside the panel

--- app/javascript/common/navbar.js
+++ app/javascript/common/navbar.js
@@ -1,9 +1,34 @@
+function hideElement(elem) {
+  elem.removeClass('show');
+  elem.addClass('hide');
+}
+
+function showElement(elem) {
+  elem.removeClass('hide');
+  elem.addClass('show');
+}
+
 $(document).on('turbolinks:load', function () {
   if ($('#navbar-collapsible')[0]) {
     $('.navbar-user-panel').on('click', function () {
-      const userLinks = $('.navbar-user-links');
-      const visibility = userLinks.css('visibility') === 'hidden' ? 'visible' : 'hidden';
-      userLinks.css('visibility', visibility);
+      const userLinks = $('.navbar-user-links.logged-in');
+      if (userLinks.hasClass('show')) {
+        hideElement(userLinks);
+      } else {
+        showElement(userLinks);
+      }
+    });
+
+    $(document).click(function (event) {
+      const target = $(event.target);
+      $('.navbar-user-links.logged-in.show').each(function () {
+        const targetInUserPanel = target.closest($('.navbar-user-panel')).length > 0;
+        const targetInUserLinks = target.closest(this).length > 0;
+
+        if (!targetInUserPanel && !targetInUserLinks) {
+          hideElement($(this));
+        }
+      });
     });
   }
 });
