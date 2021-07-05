bd2e8c858944eb97eb90dcff81fa624dbc00d118
pcfu <fupeichong@gmail.com>
2021-07-05 16:08:13 +0800

    update JS for chat page

--- app/javascript/pages/chat.js
+++ app/javascript/pages/chat.js
@@ -1,10 +1,25 @@
-export function chatFns() {
-  $('.channels-toggler').click(function () {
-    const chBar = $('.channels-bar');
-    if (chBar.hasClass('hide')) {
-      chBar.removeClass('hide');
+function initBar(barName) {
+  const bar = $(`.${barName}-bar`);
+
+  $(document).ready(function () {
+    bar.removeClass('no-transitions-on-load');
+  });
+
+  if (window.innerWidth < 768) {
+    bar.addClass('collapsed');
+  }
+
+  const toggler = $(`.${barName}-toggler`);
+  toggler.click(function () {
+    if (bar.hasClass('collapsed')) {
+      bar.removeClass('collapsed');
     } else {
-      chBar.addClass('hide');
+      bar.addClass('collapsed');
     }
   });
+}
+
+export function chatFns() {
+  initBar('channels');
+  initBar('members');
 }
