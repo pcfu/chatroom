32820ccc81970185e44c0728f16254235118b495
Peichong <fupeichong@gmail.com>
2021-07-16 08:25:43 +0800

    Create Global community on db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Community.create(
+  name: 'global',
+  description: 'A global community that all users join by default'
+)
