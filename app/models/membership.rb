c2561beb994d262c15cf0e933449dacb78d955b6
pcfu <fupeichong@gmail.com>
2021-07-22 15:48:23 +0800

    add spec for and update Membership model

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,4 +1,19 @@
 class Membership < ApplicationRecord
   belongs_to :user
   belongs_to :community
+
+  enum role: {
+    owner: 'owner',
+    administrator: 'administrator',
+    regular: 'regular',
+    banned: 'banned'
+  }
+  after_initialize :set_defaults
+
+  validates :role, presence: true
+
+
+  def set_defaults
+    self.role ||= 'regular'
+  end
 end
---
+++ spec/models/membership_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe Membership, type: :model do
+  subject(:user)        { create :user }
+  subject(:comm)        { create :community }
+
+  subject(:membership)  do
+    user.memberships.build(community: comm, role: 'regular')
+  end
+
+  it { is_expected.to be_valid }
+
+  describe "#role" do
+    it "is required" do
+      generate_blanks.each do |role|
+        membership.role = role
+        membership.valid?
+        expect(membership.errors[:role]).to include("can't be blank")
+      end
+    end
+
+    it "accepts only enum values" do
+      %w[owner administrator regular banned].each do |enum_val|
+        membership.role = enum_val
+        membership.valid?
+        expect(membership).to be_valid
+      end
+
+      expect {
+        membership.role = 'unknown'
+      }.to raise_error(ArgumentError).with_message(/is not a valid role/)
+    end
+
+    it "defaults to regular on initialization" do
+      membership = user.memberships.build(community: comm)
+      expect(membership.role).to eq('regular')
+    end
+  end
+end
